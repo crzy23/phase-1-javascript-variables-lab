4ad9c384b0c0b2f34c2081ea50f14c4f292d8017
ian ayiro <ian.ayiro@student.moringa.com>
2024-06-24 18:08:54 +0300

    first commit

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Code your solution in this file!
+let x = 1;
+
+if (x === 1) {
+  let x = 2;
+
+  console.log(x);
+  // Expected output: 2
+}
+
+console.log(x);
+// Expected output: 1
+
+let name1 = ian;
+
+let age = 21;
+
+let school = "moringa"
+
+if (age) {25
+  
+} else {notmoringastudent}
+  
+console.log(25)
+
+console.log(school)
